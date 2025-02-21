169e28b4c88afabfc2d292799f3270b5df34e67f
MugdhaRahman <mugdharahman11@gmail.com>
2025-02-21 22:09:26 +0600

    slider

---
+++ app/Component/Slider.js
@@ -0,0 +1,13 @@
+
+export default function Slider(){
+    return(
+        <div  className="flex flex-col bg-[#0D041A]  bg-[url('/slider-bg.svg')] bg-cover">
+        
+        <h1 className=" text-[92px] font-extrabold bg-gradient-to-r from-[#D9CAFF] to-[#CC7FED] bg-clip-text text-transparent mt-[322px] mb-[322px] ml-[80px]">
+            DISCOVER <br/> OUR <br/> SOLUTIONS
+        </h1>
+        
+        </div>
+
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Nav from "./Component/Nav";
 import Hero from "./Component/Hero";
+import Slider from "./Component/Slider";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Hero />
+      <Slider/>
     </>
   );
 }
